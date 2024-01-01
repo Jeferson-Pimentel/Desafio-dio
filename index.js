e4595d4c599c6833e0e72b9434bbf84fc3c2eb6a
jeferson <jefersonppimentel@hotmail.com>
2024-01-01 15:28:17 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let NomeDoHeroi ="Matador de Demonio"
+let NivelDeXp =10000
+let ClasseDoHeroi = "Imortal"
+
+switch (NivelDeXp ){case 1000 :
+console.log(" O nosso heroi " + (NomeDoHeroi) + " iniciou sua aventura. ")
+break
+
+case 2000 :
+console.log(" O heroi " + ( NomeDoHeroi) + " encontrou seu desafio.")
+break
+
+case 3000 :
+console.log(" O heroi " + ( NomeDoHeroi) + " está lutando contra demonios no terceiro nivel da masmora.")
+break
+
+case 4000 :
+console.log(" O " + (NomeDoHeroi) + " precissa se recuperar depois de se ferir gravemente.")
+break
+
+case 5000 :
+console.log(" O " + (NomeDoHeroi) + " foi convocado pela guilda de herois.")
+break
+
+case 6001 :
+console.log(" O heroi " + (NomeDoHeroi) + " subiu para o nivel ouro apos derrotar o chefe do quarto andar da masmora.")
+break
+
+case 7001 :
+console.log(" O heroi " + (NomeDoHeroi) + " formou um grupo com aventureiros de nivel ouro.")
+break
+
+case 8000 :
+console.log(" O heroi " + (NomeDoHeroi) + " não foi forte o bastante para derrotar o chefe da masmora no setimo andar.")
+break
+
+case 9001 :
+console.log(" O Heroi " + (NomeDoHeroi) + " está treinando muito para subir de nivel.")
+break
+
+case 10000:
+console.log("O heroi de nome " +  (NomeDoHeroi) + " está no nilvel de " + (ClasseDoHeroi))
+break}
